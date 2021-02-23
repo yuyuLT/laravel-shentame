774730d48e7f4d29110ef9af20a292c473156a58
yuyuLT <wawawawadaiko@icloud.com>
2021-02-24 01:28:24 +0900

    csrf追加

--- resources/views/toppage.blade.php
+++ resources/views/toppage.blade.php
@@ -25,6 +25,7 @@
                         </div>
                         <div>
                             <form method = "GET" action="{{route('toppage')}}" class="form-inline my-2 my-lg-0">
+                            @csrf
                                 <input class="form-control mr-sm-2" name="search" type="search" placeholder="検索ワードを入力" aria-label="Search">
                                 <span class="input-group-btn">
                                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">検索</button>
