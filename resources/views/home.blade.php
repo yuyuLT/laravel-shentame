3a4297d108af1228957930c5eb22771658b1ada9
yuyuLT <wawawawadaiko@icloud.com>
2021-03-11 23:01:57 +0900

    登録後の表示ページを変更

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header title_big">登録完了</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,14 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <div>ようこそ！あなたのお気に入り動画をシェアしよう！</div>
+                    <br>
+                    <form method="GET" action="{{route('toppage')}}" class="form-inline">
+                        @csrf
+                        <div class="text-right">
+                            <button type="submit" class="btn btn-primary" >動画一覧へ</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
